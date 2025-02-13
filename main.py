091d64ecce4c5359e69da874ce8961d85d149d5f
maxim-polyakov <maxim7012@mail.ru>
2025-02-13 23:49:35 +0300

    1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def parsing_tables():
     dbc = DB_Communication.DB_Communication()
     df_market = dbc.get_data('select id, name from assistant_sets.market_goods')
     df_seller = dbc.get_data('select  name, seller_name, price from assistant_sets.seller_goods')
-    d = {'id': df_market['id'], 'name': df_market['name'], 'seller_price': df_seller['price']}
+    d = {'id': df_market['id'], 'name': df_market['name'], 'seller_price': df_seller['price'], 'seller': df_seller['seller_name']}
     df2 = pd.DataFrame(data=d)
     df2 = df2.dropna()
     dbc.insert_to(df2)
